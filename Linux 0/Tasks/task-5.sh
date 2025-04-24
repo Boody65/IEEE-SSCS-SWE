079ece55e1d855b7c9d498a42e5e3e3b907fe750
Abdelrhman Ahmed <99596375+Boody65@users.noreply.github.com>
2025-04-24 14:55:57 +0200

    Delete Linux 0 directory

--- Linux 0/Tasks/task-5.sh
+++
@@ -1,7 +0,0 @@
-  270  wget https://raw.githubusercontent.com/OmarKHDR/IEEE-SSCS-SWE-2025/refs/heads/main/0x00-session/tasks-related-files/task4/numbers.txt
-  271  wget https://raw.githubusercontent.com/OmarKHDR/IEEE-SSCS-SWE-2025/refs/heads/main/0x00-session/tasks-related-files/task4/all_in_one.c
-  272  gcc all_in_one.c -o main
-  273  sudo apt install gcc
-  274  gcc all_in_one.c -o main
-  275  ./main 0> numbers.txt 1>output.txt 2>error.txt
-  276  history > task-5.sh
