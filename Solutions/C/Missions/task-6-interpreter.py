4a97cc270ae88735c0718dd99c7dddd98b76e82a
Abdelrhman Ahmed <99596375+Boody65@users.noreply.github.com>
2025-04-28 21:41:47 +0300

    Delete Solutions directory

--- Solutions/C/Missions/task-6-interpreter.py
+++
@@ -1,6 +0,0 @@
-print("Hello, world")
-x = 5
-if x < 3:
-    hi("K.O")
-else:
-    print("True")
--- Solutions/C/Tasks without context (C)/ISALPHA (task-5)/_isalpha.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ISALPHA_H
-#define _ISALPHA_H
-#include <unistd.h>
-
-int _isalpha(char c);
-
-#endif
